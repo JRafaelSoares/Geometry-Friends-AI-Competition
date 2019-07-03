712ae8877aef12330aa64bac0f59431d7658fdde
Pedro Rodrigues <pedro.p.rodrigues@tecnico.ulisboa.pt>
2019-07-03 03:31:43 +0100

    Began refactoring square agent implementation

---
+++ A-Star Current Game Implementation/GeometryFriendsAgents/Moves.cs
@@ -0,0 +1,18 @@
+﻿namespace GeometryFriendsAgents
+{
+    using System;
+
+    internal class Moves
+    {
+        public const int NO_ACTION = 0;
+        public static int ROLL_LEFT = 1;
+        public static int ROLL_RIGHT = 2;
+        public static int JUMP = 3;
+        public static int GROW = 4;
+        public static int MOVE_LEFT = 5;
+        public static int MOVE_RIGHT = 6;
+        public static int MORPH_UP = 7;
+        public static int MORPH_DOWN = 8;
+    }
+}
+
